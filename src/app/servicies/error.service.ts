1dcfce909897c755d22a62a2852f0ee3befe4b3d
Giorgos Ndinas <giorgosndinas@gmail.com>
2026-02-04 23:45:43 +0200

    Enhance ErrorService with improved method documentation

--- src/app/servicies/error.service.ts
+++ src/app/servicies/error.service.ts
@@ -13,10 +13,19 @@ export class ErrorService {
 
   readonly current = signal<AppError | null>(null);
 
+  /**
+   * Displays an error message.
+   * @param message The error message to display.
+   * @param header Optional header for the error message. Defaults to 'Error'.
+   * @param details Optional additional details about the error.
+   */
   showError(message: string, header = 'Error', details?: unknown) {
     this.current.set({ header, message, details });
   }
 
+  /**   
+   * Clears the current error message.
+   */
   clearError() {
     this.current.set(null);
   }
